6e37964e92c4e5d23e967894f031fb21710e8a8e
Blessing <eblessing793577@gmail.com>
2025-06-06 12:52:43 +0100

    Routed home and about pages in next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return(
-    <header className="bg-blue-600 text-white p-4 text-center">
+    <header className=" border-none flex justify-between bg-blue-600 text-white p-4 text-center">
     <h1 className="text-xl font-bold">ALX ProDev</h1>
+
+    <nav className="space-x-4">
+      <Link href="/home" className="hover:underline">
+        Home
+      </Link>
+      <Link href="/about" className="hover:underline">
+        About
+      </Link>
+    </nav>
   </header>
   );
 }
